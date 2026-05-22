c0925bf13e2f2682fefd9032c8d4c681f510bfc3
titan_exasaur <tinytachyon14341@gmail.com>
2026-05-22 10:14:39 +0530

    Create Project Template for Snowflake Mini-Project

---
+++ 1_snowflake_mini_project/template.sh
@@ -0,0 +1,14 @@
+# folders
+mkdir sql
+mkdir data
+mkdir notebooks
+
+# files
+touch sql/1_setup.sql
+touch sql/2_create_tables.sql
+touch sql/3_feature_engineering.sql
+touch sql/4_risk_scoring.sql
+
+touch data/shipment_data.csv
+
+touch notebooks/snowflake_ml_poc.ipynb
